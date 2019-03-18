8c8d24fb6ba4b1220c1970ca45962ae7a5f49a92
Heikki <heikki.rantala@aalto.fi>
2019-03-18 14:15:30 +0200

    battle coordinates

--- battles/battle_coordinates.py
+++ battles/battle_coordinates.py
@@ -1,12 +1,16 @@
 import csv
 from rdflib import Graph, Literal, namespace, Namespace, XSD, URIRef
 
-# meaning of columns in csv
+# this code extras place coordinates from one csv and place names from another
 
-sipa = Namespace('http://ldf.fi/siso/sita/')
+sipa = Namespace('http://ldf.fi/siso/sipa/')
+sita = Namespace('http://ldf.fi/siso/sita/')
 schema = Namespace('http://ldf.fi/siso/schema/')
+geo = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
+ecrm = Namespace('http://erlangen-crm.org/current/')
 
-def get_coordinates(c_csv, name_csv):
+
+def not_used(c_csv, name_csv):
     c_reader = csv.reader(c_csv)
     name_reader = csv.reader(name_csv)
 
@@ -16,7 +20,7 @@ def get_coordinates(c_csv, name_csv):
     #    writer = csv.writer(write_csv)
 
     place_list = list()
-    counter = 1
+    g = Graph()
 
     for row in c_reader:
         #print(row[1] + '       ' + str(int(row[0])+1).zfill(6)  + \
@@ -28,15 +32,53 @@ def get_coordinates(c_csv, name_csv):
         except:
             place_list.append(row_list)
 
+        event_uri = sita['e_' + str(int(row[0])-1).zfill(6)]
+        place_uri = sipa['bp_' + str(int(row[0])-1).zfill(6)]
+
+        g.add((place_uri, namespace.RDF.type, schema.Exact_place))
+        g.add((place_uri, namespace.SKOS.prefLabel, Literal(row[0], lang='fi')))
+        g.add((place_uri, namespace.SKOS.prefLabel, Literal('Taistelun ' + row[0], lang='fi')))
+        #g.add((sipa['bp_' + str(counter).zfill(6)], geo.lat, Literal(row[1])))
+        #g.add((sipa['bp_' + str(counter).zfill(6)], geo.long, Literal(row[2])))
+
+
+    counter = 10000
+    for row in place_list:
+        g.add((sipa['bp_' + str(counter).zfill(6)], namespace.RDF.type, schema.Exact_place))
+        g.add((sipa['bp_' + str(counter).zfill(6)], namespace.SKOS.prefLabel, Literal(row[0], lang='fi')))
+        g.add((sipa['bp_' + str(counter).zfill(6)], geo.lat, Literal(row[1])))
+        g.add((sipa['bp_' + str(counter).zfill(6)], geo.long, Literal(row[2])))
+        counter += 1
+
+    g.serialize('turtle/exact_places.ttl', format='turtle')
+
+
+def create_battle_places(c_csv, name_csv):
+    c_reader = csv.reader(c_csv)
+    name_reader = csv.reader(name_csv)
+    name_rows = list(name_reader)
     g = Graph()
 
-    for row in place_list:
-        g.add((sipa['p_' + str(counter).zfill(6)], namespace.RDF.type, schema.Exact_place))
-    print(place_list)
+    for row in c_reader:
+        index = str(int(row[0])+1)
+        event_uri = sita['e_' + index.zfill(6)]
+        place_uri = sipa['bp_' + index.zfill(6)]
 
+        # note index: name_rows[int(index)-2]
+
+        g.add((place_uri, namespace.RDF.type, schema.Battle_site))
+        g.add((place_uri, namespace.SKOS.prefLabel, Literal(name_rows[int(index)-2][2])))
+        g.add((place_uri, namespace.SKOS.altLabel, Literal("Taistelun " + name_rows[int(index) - 2][1] + " paikka")))
+        g.add((place_uri, geo.lat, Literal(row[3])))
+        g.add((place_uri, geo.long, Literal(row[4])))
+
+        g.add((event_uri, schema.exact_place, place_uri))
+
+    g.serialize('turtle/exact_places.ttl', format='turtle')
 
 
 
 with open('data/taistelupaikat.csv', 'r') as place_names:
     with open('data/taisteluiden_koordinaatit.csv', 'r') as place_coordinates:
-        get_coordinates(place_coordinates, place_names)
+        # get_coordinates(place_coordinates, place_names)
+        create_battle_places(place_coordinates, place_names)
